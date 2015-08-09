ebf1fa9ed9f52c47e901e917c2399cf1a37dcdd1
Brad Bollenbach <bradb@30sleeps.com>
2015-08-09 16:13:23 +1000

    CSS tweaks

--- js/calendar.js
+++ js/calendar.js
@@ -17,14 +17,30 @@ var Calendar = {
     return table;
   },
 
+  renderHeader: function(table, numDays) {
+    var thead = table.appendChild(document.createElement('thead')),
+        row = thead.appendChild(document.createElement('tr')),
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        th;
+
+    for (d = 0; d < numDays; d++) {
+      th = row.appendChild(document.createElement('th'));
+      th.appendChild(document.createTextNode(days[d]));
+    }
+  },
+
   showCalendar: function (containerId, numTimeslots, numDays) {
     var container = document.getElementById(containerId);
     var table = this.renderTable(container);
 
+    this.renderHeader(table, numDays);
+
     var hour = 0,
-        minute = 0;
+        minute = 0,
+        tbody = table.appendChild(document.createElement('tbody'));
+
     for(r = 0; r <= numTimeslots; r++) {
-        this.renderRow(table, hour, minute, numDays);
+        this.renderRow(tbody, hour, minute, numDays);
         minute += 15;
         if (minute > 45) { minute = 0; }
         if (minute == 0) { hour += 1; }
