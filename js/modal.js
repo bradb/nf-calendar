662252945b2908fdc7d2867ce923aefe07fa9d2f
Brad Bollenbach <bradb@30sleeps.com>
2015-08-15 10:03:24 +1000

    move modal HTML creation into calendar.calendar

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,30 @@
 var modal = (function() {
   'use strict';
 
-  var modalEl;
+  var modalEl,
+      modalHtml =
+      '<div class="modal-container" id="appointment-modal">' +
+        '<div class="modal-form">' +
+          '<h2>Appointment Date &amp; Time</h2>' +
+          '<form action="#" method="post">' +
+            '<div class="datepicker-container">' +
+              '<h4>Day</h4>' +
+              '<div id="datepicker"></div>' +
+            '</div>' +
+            '<div class="start-time-container">' +
+              '<h4>Start Time</h4>' +
+              '<div id="start-time-picker"></div>' +
+            '</div>' +
+            '<div class="end-time-container">' +
+              '<h4>End Time</h4>' +
+              '<div id="end-time-picker"></div>' +
+            '</div>' +
+            '<div class="buttons">' +
+              '<a class="button" href="#">Book Appointment</a>' +
+            '</div>' +
+          '</form>' +
+        '</div>' +
+      '</div>';
 
   var closeModal = function() {
     modalEl.style.display = "none";
@@ -20,6 +43,7 @@ var modal = (function() {
   };
 
   var modal = function(elementId) {
+    document.body.insertAdjacentHTML('beforeend', modalHtml);
     modalEl = document.getElementById(elementId);
     window.addEventListener('keydown', handleEscCancel);
     modalEl.addEventListener('click', handleClickCancel);
