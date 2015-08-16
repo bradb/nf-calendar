2b607265ec5cc55d12e64facfd1b883f0e20644c
Brad Bollenbach <bradb@30sleeps.com>
2015-08-16 12:02:56 +1000

    basic show/hide list on input

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,19 +1,51 @@
 var autocomplete = (function () {
   'use strict';
 
-  var optionsList = '<ul class="autocomplete-list"></ul>';
+  var autocompleteListClassName = 'autocomplete-list';
 
-  var optionItem = '<li class="autocomplete-item"></li>';
+  var createOption = function (value, label) {
+    var opt = document.createElement('li');
+    opt.className = 'autocomplete-item';
+    opt.dataset.value = value;
+    opt.appendChild(document.createTextNode(label));
 
-  var showCompleteOptions = function (el, searchText, options) {
-    alert("showing complete options");
+    return opt;
+  };
+
+  var removeExistingCompletionList = function (sourceEl) {
+    Array.prototype.forEach.call(
+      document.getElementsByClassName(autocompleteListClassName),
+      function (el) {
+        if (el.dataset.source === sourceEl.id) {
+          el.remove();
+        }
+      }
+    );
+  };
+
+  var showCompleteOptions = function (sourceEl, searchText, options) {
+    var list = document.createElement('ul');
+
+    removeExistingCompletionList(sourceEl);
+    if (!searchText) {
+      return;
+    }
+
+    list.dataset.source = sourceEl.id;
+
+    list.className = autocompleteListClassName;
+    options.forEach(function (opt) {
+      list.appendChild(createOption(opt[0], opt[1]));
+    });
+
+    sourceEl.parentNode.insertBefore(list, sourceEl.nextSibling);
   };
 
   var autocomplete = function (elId, options) {
     var inputEl = document.getElementById(elId);
 
     inputEl.addEventListener(
-      'keypress',
+      'keyup',
       function(event) {
         showCompleteOptions(event.target, event.target.value, options);
       });
