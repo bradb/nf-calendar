a62bf556ee1d3b5f3ce04445ce809705d23d552c
Brad Bollenbach <bradb@30sleeps.com>
2015-08-16 14:09:32 +1000

    scrolling selection

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -48,14 +48,56 @@ var autocomplete = (function () {
     sourceEl.parentNode.insertBefore(list, sourceEl.nextSibling);
   };
 
+  var downKeyPressed = function (event) {
+    return event.keyCode === 40;
+  };
+
+  var selectNextCompleteOption = function (sourceEl) {
+    var autocompleteList =
+      document.querySelector(".autocomplete-list[data-source='" + sourceEl.id + "']");
+
+    if (autocompleteList && autocompleteList.querySelector('li')) {
+      selectItem(getNextSelectableItem(autocompleteList));
+    }
+  };
+
+  var getNextSelectableItem = function (list) {
+    var currentSelectedItem = list.querySelector("li.selected");
+
+    if (currentSelectedItem && currentSelectedItem.nextSibling) {
+      return currentSelectedItem.nextSibling;
+    } else {
+      return list.querySelector('li:first-child');
+    }
+  };
+
+  var selectItem = function (item) {
+    Array.prototype.map.call(
+      item.parentNode.querySelectorAll('li'),
+      function (el) {
+        el.classList.remove('selected');
+    });
+
+    item.classList.add('selected');
+  };
+
   var autocomplete = function (elId, options) {
     var inputEl = document.getElementById(elId);
 
     inputEl.addEventListener(
-      'keyup',
+      'input',
       function(event) {
         showCompleteOptions(event.target, event.target.value, options);
       });
+
+    inputEl.addEventListener(
+      'keydown',
+      function(event) {
+        if (downKeyPressed(event)) {
+          selectNextCompleteOption(event.target);
+        }
+      }
+    );
   };
 
   return { autocomplete: autocomplete };
